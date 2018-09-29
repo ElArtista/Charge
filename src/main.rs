2fc50f6b1367eabedd037a6ce429425e181d19e9
Agorgianitis Loukas <agorglouk@gmail.com>
2018-09-29 13:45:53 +0300

    Integrated game and mainloop modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate time;
+
+mod game;
+mod mainloop;
+
+use game::Game;
+use mainloop::MainLoop;
+
 fn main() {
     println!("Hello, world!");
+    let mut game = Game::new();
+    game.init();
+    {
+        let mut mainloop = MainLoop::new(
+            Box::new(|mut game, dt| Game::update(&mut game, dt)),
+            Box::new(|game, interpolation| Game::render(&game, interpolation)),
+            Some(Box::new(|game, ms, fps| Game::perf(&game, ms, fps))),
+            &mut game,
+        );
+        mainloop.run();
+    }
+    game.shutdown();
 }
